cdb583a61ad5891f4be7d0f7bde003eb00b14793
gGonz <zurg.cei@gmail.com>
2013-06-05 17:08:15 -0500

    Improves setup script

--- setup.py
+++ setup.py
@@ -1,28 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 from setuptools import setup, find_packages
 
-METADATA = dict(
+from socialnetwork import __version__
+
+
+setup(
     name='django-socialnetwork',
-    version='0.0.3',
+    version=__version__,
+    description=('Provides functionality to login and share with social '
+        'networks to Django.'),
+    long_description=open(
+        os.path.join(os.path.dirname(__file__), 'README.rst')
+    ).read(),
     author=u'Gabriel González',
     author_email='zurg.cei@gmail.com',
-    description=('Django app that provides login and share access with '
-        'social networks.'),
-    long_description=open('README.rst').read(),
-    license='Apache License (2.0)',
     url='https://github.com/gGonz/django-socialnetwork',
+    license='Apache License (2.0)',
     install_requires=[
         'Django >= 1.4',
         'requests',
         'requests_oauthlib',
     ],
-    include_package_data=True,
     packages=find_packages(),
+    include_package_data=True,
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+	'License :: OSI Approved :: Apache Software License',
+        'Environment :: Web Environment',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+	'Topic :: Software Development :: Libraries :: Python Modules',
+	'Topic :: Internet',
+
+    ]
 )
-
-
-if __name__ == '__main__':
-    setup(**METADATA)
-
 
